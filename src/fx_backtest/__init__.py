d6aac1a7f9859ec3bfa9cd167944139e924cd22f
zhangxingthang97-lang <zhangxingthang97@gmail.com>
2026-09-08 16:25:24 +0800

    Add src/fx_backtest/__init__.py

---
+++ src/fx_backtest/__init__.py
@@ -0,0 +1,6 @@
+"""Reproducible BTCUSDT London-session breakout research package."""
+
+from .london_breakout import BreakoutConfig, backtest_symbol, position_units
+
+__all__ = ["BreakoutConfig", "backtest_symbol", "position_units"]
+
