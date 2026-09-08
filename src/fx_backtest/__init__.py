884b0aa7500bdec87503c468d2b040cb2f0f5f4d
zhangxingthang97-lang <zhangxingthang97@gmail.com>
2026-09-08 17:05:39 +0800

    Remove superseded src/fx_backtest/__init__.py

--- src/fx_backtest/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reproducible BTCUSDT London-session breakout research package."""
-
-from .london_breakout import BreakoutConfig, backtest_symbol, position_units
-
-__all__ = ["BreakoutConfig", "backtest_symbol", "position_units"]
-
